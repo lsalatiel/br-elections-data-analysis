5bc81bcc6b73abb88581a0c778fa6d5e9fbf82fb
lsalatiel <lucassalatiel1004@gmail.com>
2023-12-13 02:32:12 -0300

    add candidato functions

--- src/electionslib/candidato.cpp
+++ src/electionslib/candidato.cpp
@@ -1,7 +1,7 @@
 #include "include/candidato.h"
 
-Candidato::Candidato(std::string nome, std::string nome_urna, int codigo_situacao_candidato, int numero, int numero_partido, int numero_federacao,
-        int codigo_situacao_turno, bool genero, std::string nome_tipo_dest_votos, int total_votos) :
+Candidato::Candidato(std::string nome, std::string nome_urna, Candidato::SituacaoCandidato codigo_situacao_candidato, int numero, int numero_partido, int numero_federacao,
+        Candidato::SituacaoTurno codigo_situacao_turno, Candidato::Genero genero, Candidato::TipoDestinoVoto nome_tipo_dest_votos, int total_votos) :
             nome(nome),
             nome_urna(nome_urna),
             codigo_situacao_candidato(codigo_situacao_candidato),
@@ -13,4 +13,53 @@ Candidato::Candidato(std::string nome, std::string nome_urna, int codigo_situaca
             nome_tipo_dest_votos(nome_tipo_dest_votos),
             total_votos(total_votos) {}
 
+int Candidato::get_numero_federacao() {
+    return numero_federacao;
+}
+
+int Candidato::get_numero_candidato() {
+    return numero;
+}
+
+int Candidato::get_quantidade_votos() {
+    return total_votos;
+}
+
+Candidato::TipoDestinoVoto Candidato::get_nome_tipo_dest_votos() {
+    return nome_tipo_dest_votos;
+}
+
+/* LocalDate getDataNascimento(); */
+
+std::string Candidato::get_nome_na_urna() {
+    return nome_urna;
+}
+
+std::string Candidato::get_nome() {
+    return nome;
+}
+
+Candidato::Genero Candidato::get_genero() {
+    return genero;
+}
+
+Candidato::SituacaoCandidato Candidato::get_codigo_situacao_candidato() {
+    return codigo_situacao_candidato;
+}
+
+int Candidato::get_numero_partido() {
+    return numero_partido;
+}
+
+bool Candidato::is_candidatura_deferida() {
+    return codigo_situacao_candidato == Candidato::SituacaoCandidato::DEFERIDO;
+}
+
+bool Candidato::is_eleito() {
+    return codigo_situacao_turno == Candidato::SituacaoTurno::ELEITO;
+}
+
+void Candidato::add_quantidade_votos(int quantidadeVotos) {
+    total_votos += quantidadeVotos;
+}
 
--- src/electionslib/include/candidato.h
+++ src/electionslib/include/candidato.h
@@ -4,26 +4,56 @@
 #include <iostream>
 #include <chrono>
 
-const bool MASCULINO = true;
-const bool FEMININO = false;
-
 class Candidato {
-    std::string nome;
-    std::string nome_urna;
-    int codigo_situacao_candidato; // 2 ou 16 = candidato deferido
-    int numero;
-    int numero_partido;
-    int numero_federacao;
-    int codigo_situacao_turno; // 2 ou 3 = eleito
-    bool genero;
-    std::chrono::year_month_day data_nascimento;
-    std::string nome_tipo_dest_votos; // nominal ou legenda
-    int total_votos;
+    public:
+        enum class Genero { MASCULINO, FEMININO };
+        enum class TipoDestinoVoto { NOMINAL, LEGENDA };
+        enum class SituacaoCandidato { DEFERIDO, INDEFERIDO };
+        enum class SituacaoTurno { ELEITO, NAO_ELEITO };
+
+    private:
+        std::string nome;
+        std::string nome_urna;
+        SituacaoCandidato codigo_situacao_candidato; // 2 ou 16 = candidato deferido
+        int numero;
+        int numero_partido;
+        int numero_federacao;
+        SituacaoTurno codigo_situacao_turno; // 2 ou 3 = eleito
+        Genero genero;
+        /* std::chrono::year_month_day data_nascimento; */
+        TipoDestinoVoto nome_tipo_dest_votos; // nominal ou legenda
+        int total_votos;
 
     public:
         Candidato();
-        Candidato(std::string nome, std::string nome_urna, int codigo_situacao_candidato, int numero, int numero_partido, int numero_federacao,
-                int codigo_situacao_turno, bool genero, std::string nome_tipo_dest_votos, int total_votos);
+        Candidato(std::string nome, std::string nome_urna, Candidato::SituacaoCandidato codigo_situacao_candidato, int numero, int numero_partido, int numero_federacao,
+                Candidato::SituacaoTurno codigo_situacao_turno, Candidato::Genero genero, Candidato::TipoDestinoVoto nome_tipo_dest_votos, int total_votos);
+
+        int get_numero_federacao();
+
+        int get_numero_candidato();
+
+        int get_quantidade_votos();
+
+        Candidato::TipoDestinoVoto get_nome_tipo_dest_votos();
+
+        bool is_candidatura_deferida();
+
+        bool is_eleito();
+
+        /* LocalDate getDataNascimento(); */
+
+        std::string get_nome_na_urna();
+
+        std::string get_nome();
+
+        Candidato::Genero get_genero();
+
+        Candidato::SituacaoCandidato get_codigo_situacao_candidato();
+
+        void add_quantidade_votos(int quantidadeVotos);
+
+        int get_numero_partido();
 };
 
 #endif
