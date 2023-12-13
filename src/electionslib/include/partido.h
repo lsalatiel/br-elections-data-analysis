d24d38aa885afb95224000f7227f125570d7303a
lsalatiel <lucassalatiel1004@gmail.com>
2023-12-13 02:32:43 -0300

    add partido functions

--- src/electionslib/include/partido.h
+++ src/electionslib/include/partido.h
@@ -2,18 +2,44 @@
 #define _PARTIDO_H_
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include "candidato.h"
 
 class Partido {
     std::string sigla;
-    std::string numero;
-    int numero_federacao;
+    int numero;
     int votos_nominais;
     int votos_legenda;
-    // map <std::string, Candidato> candidatos
+    std::map<int, Candidato &> candidatos;
 
     public:
         Partido();
-        Partido(std::string sigla, std::string numero);
+        Partido(std::string sigla, int numero);
+
+        int get_numero();
+
+        std::string get_sigla();
+
+        Candidato &get_candidato(int numeroCandidato);
+
+        void add_candidato(Candidato &c);
+
+        std::vector<Candidato> get_candidato_vector();
+
+        int get_votos_legenda();
+
+        int get_votos_nominais();
+
+        int get_votos_totais();
+
+        void add_votos_legenda(int votosLegenda);
+
+        void add_votos_nominais(int qtdVotos);
+
+        Candidato &get_candidato_mais_votado();
+
+        Candidato &get_candidato_menos_votado();
 };
 
 #endif
--- src/electionslib/partido.cpp
+++ src/electionslib/partido.cpp
@@ -1,10 +1,60 @@
 #include "include/partido.h"
 
-Partido::Partido(std::string sigla, std::string numero) :
+Partido::Partido(std::string sigla, int numero) :
             sigla(sigla),
             numero(numero) {}
 
+int Partido::get_numero() {
+    return numero;
+}
 
-void addCandidato(Candidato c){
-    //TO DO
+std::string Partido::get_sigla() {
+    return sigla;
+}
+
+void Partido::add_candidato(Candidato &c) {
+    candidatos[c.get_numero_candidato()] = c;
+}
+
+Candidato &Partido::get_candidato(int numero_candidato) {
+    /* if(candidatos.find(numero_candidato) == candidatos.end()) */
+    /*     throw std::invalid_argument("Candidato não encontrado"); */
+    return candidatos[numero_candidato];
+}
+
+std::vector<Candidato> Partido::get_candidato_vector() {
+    std::vector<Candidato> candidatos_vector;
+    candidatos_vector.reserve(candidatos.size());
+    for(const auto &[key, value] : candidatos) {
+        candidatos_vector.push_back(value);
+    }
+    return candidatos_vector;
+}
+
+int Partido::get_votos_legenda() {
+    return votos_legenda;
+}
+
+int Partido::get_votos_nominais() {
+    return votos_nominais;
+}
+
+int Partido::get_votos_totais() {
+    return votos_legenda + votos_nominais;
+}
+
+void Partido::add_votos_legenda(int qtd_votos) {
+    votos_legenda += qtd_votos;
+}
+
+void Partido::add_votos_nominais(int qtd_votos) {
+    votos_nominais += qtd_votos;
+}
+
+Candidato &Partido::get_candidato_mais_votado() {
+
+}
+
+Candidato &Partido::get_candidato_menos_votado() {
+
 }
