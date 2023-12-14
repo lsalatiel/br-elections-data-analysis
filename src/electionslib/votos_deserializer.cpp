435fafde68d6736464a799c68d258ea02bce42c2
naascimento0 <gabrieloliveiran@hotmail.com>
2023-12-14 16:53:14 -0300

    labgrad2

--- src/electionslib/votos_deserializer.cpp
+++ src/electionslib/votos_deserializer.cpp
@@ -66,15 +66,25 @@ static void processa_votos(std::map<int, Partido> partidos, int cargo, std::stri
                 if(numero_candidato == 95 || numero_candidato == 96 || numero_candidato == 97 || numero_candidato == 98)
                     continue;
                 
+                std::string numero_partido = std::to_string(numero_candidato).substr(0, 2);
                 if(cargo_candidato == cargo){
                     if(partidos.find(numero_partido) == partidos.end())
-                       // partidos[numeroCandidato].addVotosLegenda(votos);
-                    
+                       // partidos[numeroCandidato].addVotosLegenda(votos); //usar substring para pegar apenas os dois primeiros numeros do candidato ?  else{
+                        // std::string numeroPartido = numeroCandidato; //aqui tem que fazer substring, porem e int
+                    }//da pra literalmente fazer uma funcao em candidato getNumeroPartido e retornar os dois primeuiros numero
+                        //se for deptuado estadual, divide por 1000. federal divide por 100
                 }
-                
+                // olha isso aqui :
+                // int valor = 45642;
+
+                // Converte o valor inteiro em uma string.
+                std::string numero_partido = std::to_string(valor).substr(0, 2);
+
+                // Extrai os dois primeiros dígitos da string.
+                // std::string doisPrimeirosNumeros = stringValor.substr(0, 2);
             }catch(std::exception &e){
                 e.what();
-            }
+            } // BLZ VO COMITAR
         }
     }catch(std::exception &e){
         e.what();
